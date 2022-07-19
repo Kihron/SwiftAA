390a099f5bd2b497dae9694c75a6c1b6d0e26e12
Kihron <starlicblackhole@gmail.com>
2022-07-19 10:15:41 -0400

    Initial Commit

---
+++ SwiftAA/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftAA
+//
+//  Created by Dominic Thompson on 7/19/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftAA/SwiftAAApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftAAApp.swift
+//  SwiftAA
+//
+//  Created by Dominic Thompson on 7/19/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftAAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
